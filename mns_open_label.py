0bac513b743720fe800d56dce8a44bff6e0b7659
David Song <david.song@wustl.edu>
2021-11-12 15:33:47 -0600

    updates

--- mns_open_label.py
+++ mns_open_label.py
@@ -2,8 +2,13 @@ from datetime import datetime, timedelta, timezone
 from twilio.rest import Client
 import random
 
+#Real
 account_sid = 'AC1d2345f41654c309d21a2fe29b980973'
-auth_token = '8c4f623f07a2fc394772a8678017993f'
+auth_token = '00f5137ee397f165d2bb716af2485070'
+
+#Trial
+#account_sid = 'AC6762650c8e7924d2c4b23fbbe5d9e59d'
+#auth_token = '4e97f341f0fbf8abc3f75e66908a724e'
 
 client = Client(account_sid, auth_token)
 
@@ -14,7 +19,8 @@ def generate_time(start, end):
     )
     return rand_time
 
-def send_text(to):
-    client.messages.create(from_='+13148041421', to=to, body='MNS Study: Please fill out the linked survey https://forms.gle/rYBJ55D2me8rxHqx5')
-
-
+def send_text(to, on, off):
+    #Real
+    client.messages.create(from_='+13148041421', to=to, body=f'MNS Study\nMy device is ON: {on}\nMy device is Off: {off}\nQuestions? 314-362-2083')
+    #Fake
+    #client.messages.create(from_='+13185268981', to=to, body='MNS Study: Please fill out the linked survey https://forms.gle/rYBJ55D2me8rxHqx5')
--- schedule_text.py
+++ schedule_text.py
@@ -2,21 +2,22 @@ from datetime import datetime, timedelta, timezone
 from redis import Redis
 from twilio.rest import Client
 from rq_scheduler import Scheduler
+import pandas as pd
 
 import mns_open_label
 
 scheduler = Scheduler(connection=Redis()) # Get a scheduler for the "default" queue
 
-with open('/home/numbers.txt') as f:
-    numbers = f.readlines()
-    numbers = [num.rstrip() for num in numbers]
-
+contact = pd.read_csv('/home/contact_info.csv')
 
 d1 = datetime.strptime(str(datetime.now().date())+' 2:00 PM', '%Y-%m-%d %I:%M %p')
 d2 = datetime.strptime(str(datetime.now().date() + timedelta(days=1)) +' 2:00 AM', '%Y-%m-%d %I:%M %p')
 
-for num in numbers:
+for index, row in contact.iterrows():
+    num = str(row['Number'])
+    on_link = row['On']
+    off_link = row['Off']
     scheduled_time = mns_open_label.generate_time(d1, d2)
     print(f'Texted {num} at {scheduled_time}')
     if scheduled_time:
-        scheduler.enqueue_at(scheduled_time, mns_open_label.send_text, str(num))
+        scheduler.enqueue_at(scheduled_time, mns_open_label.send_text, num, on_link, off_link)
